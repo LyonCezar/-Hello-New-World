3428795349038524ba24774bc0b88652f2f32c3f
LyonCezar <lyoncezarborges@gmail.com>
2021-08-31 00:21:44 -0300

    Inserindo HTML dentro do JavaScript com Elementos

---
+++ Exec2/main.js
@@ -0,0 +1,17 @@
+console.log('testando')
+
+let div = document.querySelector('.content')
+console.log(div)
+
+let div2 = document.createElement('p')
+div2.innerHTML = 'testando 2'
+
+div.appendChild(div2)
+
+let div3 = document.createElement('div3')
+
+div.appendChild(div3)
+
+
+//elemento.style.background = 'green'
+//elemento.style.color = 'white'
